f20d2bb6437f465eacdd4dedccf195071d65ea2a
palwhite <dt19abn@herts.ac.uk>
2024-04-07 17:10:59 +0100

    my first answer push

---
+++ homework6/src/Question1.java
@@ -0,0 +1,13 @@
+public class Question1 {
+    public static void main(String[] args) {
+        Question1 q1 = new Question1();
+        q1.sum();
+
+    }
+    public void sum(){
+        int x = 74;
+        int y = 36;
+        int sum = x+y;
+        System.out.println("Expected Output: "+sum);
+    }
+}
