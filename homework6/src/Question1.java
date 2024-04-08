bd8170633f7627ce13ebd4484a4d054c6dfe3ab8
palwhite <dt19abn@herts.ac.uk>
2024-04-08 12:28:22 +0100

    My first Answer

--- homework6/src/Question1.java
+++ homework6/src/Question1.java
@@ -1,4 +1,11 @@
 public class Question1 {
+
+    /*
+    Write a Java program to print the sum of two numbers.
+    Test Data: 74 + 36
+    Expected Output: 110
+     */
+
     public static void main(String[] args) {
         Question1 q1 = new Question1();
         q1.sum();
@@ -8,6 +15,7 @@ public class Question1 {
         int x = 74;
         int y = 36;
         int sum = x+y;
+        System.out.println("Test Data: 74 + 36 ");
         System.out.println("Expected Output: "+sum);
     }
 }
