db88071ed1ee82c5d973a43bb3e09bc45949c992
palwhite <dt19abn@herts.ac.uk>
2024-04-10 11:03:30 +0100

    Week 6 Answeres

--- homework6/src/W6q15.java
+++ homework6/src/W6q15.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 /*15. Write a Java program to convert a given string into lowercase.
     Sample Input: THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
     Output: the quick brown fox jumps over the lazy dog.*/
@@ -7,7 +5,7 @@ public class W6q15 {
     public void lower(){
         String upc;
         upc="THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.";
-        Scanner s=new Scanner(System.in);
+        //Scanner s=new Scanner(System.in);
 
        /* System.out.println("Enter String to Convert in UpperCase: " );
         upc=s.nextLine();*/
